3dcca1314596eb5843f6e2847d1476fc9de39bce
Luiz Faias <luizfaias@gmail.com>
2015-02-23 16:06:09 +0000

    minor output change

--- delete_inactive_views.rb
+++ delete_inactive_views.rb
@@ -1,7 +1,7 @@
 require 'zendesk_api'
 require 'commander/import'
 
-program :version, '0.1'
+program :version, '0.2'
 program :description, 'This script allows you to delete all inactive shared (not personal) views'
 program :help, 'Author', 'Luiz Faias <lfaias@zendesk.com>'
 default_command :run
@@ -21,6 +21,7 @@ command :run do |c|
     url       = ask "URL (e.g https://{subdomain}.zendesk.com/api/v2): "
     email     = ask "Email (e.g admin@company.com): "
     api_token = ask ("API Token: ") { |q| q.echo = "*" }
+    say "\n"
 
     client = ZendeskAPI::Client.new do |config|
       config.url = url
@@ -35,7 +36,7 @@ command :run do |c|
       begin
         unless view.restriction
           unless view.active
-            puts "\nDeleting view ##{view['id']}: ##{view['title']}"
+            puts "Deleting view ##{view['id']}: ##{view['title']}"
             client.connection.delete("views/#{view['id']}.json")
             count += 1
           end
